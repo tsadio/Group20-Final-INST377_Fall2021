8168e8a16a7f2a67710533186afbca55c4f68f34
marilynpothen <m3pothen@gmail.com>
2021-11-21 22:28:40 -0500

    fixing mpothen branch issue
    deleted my changes, so i can make them again in my own mpothen branch

--- controllers/vaccineDataCounty.js
+++
@@ -1,8 +0,0 @@
-const vaccineDataCounty = `SELECT county_ID, 
-    first_dose_count, 
-    first_dose_prop,
-    second_dose_count 
-    second_dose_prop 
-FROM 
-    vaccin_data_by_county`;
-export default vaccineDataCounty;
